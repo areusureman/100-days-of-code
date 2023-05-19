637621615dfd640b68a26daafadbebccdfd64296
areyousureman <arshmanjakie1@gmail.com>
2023-05-18 23:23:12 -0400

    Create Username_Generator.py

---
+++ Day 1/Username_Generator.py
@@ -0,0 +1,9 @@
+#  Introduction
+print("Welcome, I heard you are trying to come up with a username!")
+# input variable to get a response, but also make that input a variable
+# /n is a line break
+emotion = input("How do you feel in this current moment?\n")
+# followup question
+thing = input("Quick! Think of a random object!\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("your username is " + "Xx" + emotion +"_" + thing + "xX" )
